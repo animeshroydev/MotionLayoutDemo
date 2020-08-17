7e7f72bfedc21cfd550248275cb089a87a090b77
animesh <royanimesh2211@gmail.com>
2020-08-17 11:24:11 +0530

    Starter commit

---
+++ app/src/main/java/com/animeshroy/android/sports/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.animeshroy.android.sports
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.android.behappy.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
